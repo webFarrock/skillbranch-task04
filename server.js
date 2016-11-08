a871d1f13a914cc1034f07b8a4a64e51ee2d84b3
webFarrock <webFarrock@mail.ru>
2016-11-08 11:13:39 +0300

    fin

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
+var cors = require('cors');
 var app = express();
-
+app.use(cors());
 app.get('/', function(req, res) {
 
     var fullname = req.query.fullname || '';
@@ -9,12 +10,14 @@ app.get('/', function(req, res) {
     var lastName = '';
     var name = '';
     var secondName = '';
-    var result = '';
-    if(arFullname.length > 3){
-        result = 'Invalid fullname';
+
+
+    if(arFullname.length > 3 || !fullname || /\d/.test(fullname) || /_/.test(fullname)|| /\//.test(fullname)){
+        res.send('Invalid fullname');
     }else{
+
         if(arFullname.length)
-            lastName = arFullname.pop()+' ';
+            lastName = arFullname.pop();
 
         if(arFullname.length)
             secondName =  arFullname.pop();
@@ -23,9 +26,22 @@ app.get('/', function(req, res) {
             name =  arFullname.pop();
 
 
-        if(name) name = name.substring(0,1)+'. ';
+        if(name){
+            name = name.substring(0,1)+'.';
+        }
 
-        if(secondName) secondName = secondName.substring(0,1)+'. ';
+        if(secondName){
+            if(name)
+                name += ' ';
+
+            secondName = secondName.substring(0,1)+'.';
+        }
+
+        if(name || secondName){
+            lastName +=' ';
+        }
+
+
 
         result = lastName + name + secondName
     }
